04e451435eacfc9f9093e9c752019845eb326d9c
Mr Laali <mohammadrezalaali@gmail.com>
2026-05-30 18:58:43 +0200

    Add first experiment screen design

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,291 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const EmotionStudyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class EmotionStudyApp extends StatelessWidget {
+  const EmotionStudyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Emotion Classification Study',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Arial',
+        colorScheme: const ColorScheme.light(
+          primary: Colors.black,
+          surface: Colors.white,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ExperimentScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ExperimentScreen extends StatefulWidget {
+  const ExperimentScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ExperimentScreen> createState() => _ExperimentScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ExperimentScreenState extends State<ExperimentScreen> {
+  final List<String> sentences = [
+    'I could not stop smiling after hearing the news.',
+    'Everything felt heavy and empty today.',
+    'I cannot believe they treated me like that.',
+    'I felt something terrible was about to happen.',
+  ];
+  
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  final List<String> emotions = [
+    'Anger',
+    'Fear',
+    'Happiness',
+    'Sadness',
+  ];
+
+  int currentIndex = 0;
+  int timeLeft = 15;
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  void startTimer() {
+    timer?.cancel();
+    timeLeft = 15;
+
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (timeLeft <= 1) {
+        timer.cancel();
+        nextSentence();
+      } else {
+        setState(() {
+          timeLeft--;
+        });
+      }
     });
+  }
+
+  void selectEmotion(String emotion) {
+    // Later we will save:
+    // sentence, target emotion, chosen emotion, reaction time, participant id
+
+    nextSentence();
+  }
+
+  void nextSentence() {
+    if (currentIndex < sentences.length - 1) {
+      setState(() {
+        currentIndex++;
+      });
+      startTimer();
+    } else {
+      timer?.cancel();
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) => const FinishScreen(),
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final progress = (currentIndex + 1) / sentences.length;
+    final timerProgress = timeLeft / 15;
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+      body: SafeArea(
+        child: Stack(
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Positioned(
+              top: 24,
+              left: 24,
+              child: SizedBox(
+                width: 52,
+                height: 52,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    CircularProgressIndicator(
+                      value: timerProgress,
+                      strokeWidth: 3,
+                      backgroundColor: Colors.black12,
+                      valueColor:
+                          const AlwaysStoppedAnimation<Color>(Colors.black),
+                    ),
+                    Text(
+                      '$timeLeft',
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Positioned(
+              top: 42,
+              left: 120,
+              right: 120,
+              child: Column(
+                children: [
+                  LinearProgressIndicator(
+                    value: progress,
+                    minHeight: 3,
+                    backgroundColor: Colors.black12,
+                    valueColor:
+                        const AlwaysStoppedAnimation<Color>(Colors.black),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    '${currentIndex + 1} / ${sentences.length}',
+                    style: const TextStyle(
+                      fontSize: 13,
+                      letterSpacing: 0.4,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 820),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        sentences[currentIndex],
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 34,
+                          height: 1.35,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: -0.4,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(height: 64),
+                      Wrap(
+                        spacing: 22,
+                        runSpacing: 22,
+                        alignment: WrapAlignment.center,
+                        children: emotions.map((emotion) {
+                          return EmotionButton(
+                            label: emotion,
+                            onTap: () => selectEmotion(emotion),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class EmotionButton extends StatefulWidget {
+  final String label;
+  final VoidCallback onTap;
+
+  const EmotionButton({
+    super.key,
+    required this.label,
+    required this.onTap,
+  });
+
+  @override
+  State<EmotionButton> createState() => _EmotionButtonState();
+}
+
+class _EmotionButtonState extends State<EmotionButton> {
+  bool isHovered = false;
+  bool isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final active = isHovered || isPressed;
+
+    return MouseRegion(
+      onEnter: (_) => setState(() => isHovered = true),
+      onExit: (_) => setState(() {
+        isHovered = false;
+        isPressed = false;
+      }),
+      child: GestureDetector(
+        onTapDown: (_) => setState(() => isPressed = true),
+        onTapUp: (_) {
+          setState(() => isPressed = false);
+          widget.onTap();
+        },
+        onTapCancel: () => setState(() => isPressed = false),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 180),
+          curve: Curves.easeOut,
+          width: 150,
+          height: 150,
+          decoration: BoxDecoration(
+            color: active ? Colors.black : Colors.white,
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.black, width: 1.4),
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            widget.label,
+            style: TextStyle(
+              color: active ? Colors.white : Colors.black,
+              fontSize: 17,
+              fontWeight: FontWeight.w500,
+              letterSpacing: 0.2,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FinishScreen extends StatelessWidget {
+  const FinishScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Thank you for participating.',
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.w400,
+            color: Colors.black,
+          ),
+        ),
       ),
     );
   }
